51f252e40fa8ed4af19647f53d20711d1a2d5da8
Sumiran <sumiran.b@cisinlabs.com>
2025-01-15 17:42:03 +0530

    initial Code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello Sumiran, Welcome to DevOps
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
